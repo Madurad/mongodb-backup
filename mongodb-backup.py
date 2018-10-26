5a5e3204cb5146a2b59cee322ed2c43902a3e9d4
Madura <madura@Madura-Mac.local>
2018-10-26 14:15:56 +0530

    added backup loaction

--- mongodb-backup.py
+++ mongodb-backup.py
@@ -2,7 +2,7 @@ import subprocess
 import shellx
 
 #Add DB and the collection you want to backup
-command = shlex.split("mongodump  --db <dbName> --collection <collectionName>")
+command = shlex.split("mongodump  --db <dbName> --collection <collectionName> /path/to/backup/location/")
 
 process = subprocess.Popen(command, stdout=subprocess.PIPE)
 output, err = process.communicate()
